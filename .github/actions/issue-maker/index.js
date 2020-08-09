9dcb3f2f93ed42e8d96014baaa0458ae52886a2d
ycsgdsg <411469813@qq.com>
2020-08-09 17:29:38 +0800

    Update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,1 +1,23 @@
-console.log("haha")
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function run() {
+  try {
+    const issueTitle = core.getInput("issue-title");
+    const jokeBody = core.getInput("joke");
+    const token = core.getInput("repo-token");
+
+    const octokit = new github.GitHub(token);
+
+    const newIssue = await octokit.issues.create({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
